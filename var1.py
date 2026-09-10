814b3703e82cb092b40ef265d81ef59095aa4913
Kartik Shinde <kartik.ashok.shinde.03@gmail.com>
2026-09-10 05:33:22 +0530

    aaj ka code

---
+++ var1.py
@@ -0,0 +1,6 @@
+name  = "kartik"
+age  = 20
+city  = "nanded"
+print(name )
+print(age)
+print(city)
---
+++ var2.py
@@ -0,0 +1,12 @@
+x = 3
+y = 7
+
+print("Swap se PEHLE:")
+print("x =", x)
+print("y =", y)
+
+x, y = y, x
+
+print("Swap ke BAAD:")
+print("x =", x)
+print("y =", y)
